cb166b3f0f828dde14029a93aea968404b96781c
Tim Schwab <timothy.john.schwab@gmail.com>
2021-11-22 13:55:53 -0500

    hash pass

--- src/redis-layer/users.js
+++ src/redis-layer/users.js
@@ -1,7 +1,13 @@
+const bcrypt = require('bcrypt');
 const client = require('./redis-client.js');
 
+const saltRounds = 10;
+
 async function create(name, pass) {
-	await client.set(name, pass);
+	let userKey = 'user|' + name;
+	let pass = await bcrypt.hash(pass, saltRounds);
+
+	await client.set(userKey, pass);
 }
 
 module.exports.create = create;
