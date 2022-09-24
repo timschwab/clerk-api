d7d07599ab7c7c40b418e631d6bdc1073e79800b
pyzaist <timothy.john.schwab@gmail.com>
2022-09-24 12:37:08 -0400

    set as exe and try again

--- run.js
+++ run.js
@@ -1,11 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const router = require('./src/router.js');
+const router = require("./src/router.js");
 
 const port = 8357;
 
-app.use('/', router);
+app.use("/", router);
 
-app.listen(port, () => console.log('Site running on ' + port));
-
+app.listen(port, () => console.log("Site running on " + port));
