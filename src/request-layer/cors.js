e2161d57bd00dfdccc80f1a8111caf9fdc49d1d3
pyzaist <timothy.john.schwab@gmail.com>
2022-12-23 00:55:46 -0500

    convince the browser to cache the preflights

--- src/request-layer/cors.js
+++ src/request-layer/cors.js
@@ -1,10 +1,13 @@
+const secondsInADay = 60 * 60 * 24;
+
 function middleware(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "*");
+	res.header("Access-Control-Allow-Headers", "Authorization, *");
 	res.header("Access-Control-Allow-Methods", "*");
+	res.header("Access-Control-Max-Age", secondsInADay);
 
 	if (req.method == "OPTIONS") {
-		res.status(200).send();
+		res.status(204).send();
 	} else {
 		next();
 	}
